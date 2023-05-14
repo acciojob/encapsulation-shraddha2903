01d38bc95003265daaf369761f63fe759eaa53d9
shraddha2903 <70905602+shraddha2903@users.noreply.github.com>
2023-05-14 11:12:38 +0530

    task 6, setting and getting name from main function

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String args[])
     {
         RWOnly obj = new RWOnly();
+        //setter
+        obj.setName("Shraddha Singh");
+        //getter
+        System.out.println(obj.getName());
         //obj.name = "Shraddha";
         //System.out.println(obj.name);
         //name has private access in 'com.driver.RWOnly'
