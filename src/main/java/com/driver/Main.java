736675a7a8f969663f7263aea13e2bda688910ea
shraddha2903 <70905602+shraddha2903@users.noreply.github.com>
2023-05-14 10:29:11 +0530

    Task 4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String args[])
+    {
+        RWOnly obj = new RWOnly();
+        //obj.name = "Shraddha";
+        //name has private access in 'com.driver.RWOnly'
+
+    }
+
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,6 @@
 package com.driver;
 
 public class RWOnly {
+    private String name;
     
 }
